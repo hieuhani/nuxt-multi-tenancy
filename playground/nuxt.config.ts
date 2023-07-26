7f300f718254e13b3e27cb54ea5427af791d59dd
Hieu Tran <hieutran.fu@gmail.com>
2023-07-26 11:52:29 +0700

    add GA to playground site

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,7 +1,11 @@
 export default defineNuxtConfig({
-  modules: ["../src/module"],
+  modules: ["../src/module", "nuxt-gtag"],
   multiTenancy: {
     rootDomains: ["nuxtdev.local", "nuxtdev.xyz"],
   },
   devtools: { enabled: true },
+  gtag: {
+    id: "G-SPYZPSKBM8",
+    initialConsent: false,
+  },
 });
