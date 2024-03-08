953b8febefdc1d0e94aebe7ec46b201c42b1ae07
Hieu Tran <hieutran.fu@gmail.com>
2024-03-08 21:56:36 +0700

    update readme

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="articles-wrapper">
+    <a :href="jobLink" class="jobs-link">Jobs</a>
     <article-item
       v-for="article in articles"
       :key="article.id"
@@ -9,8 +10,13 @@
 </template>
 
 <script setup lang="ts">
-import { useFetch } from "#imports";
+import { useFetch, useAppConfig } from "#imports";
 import type { Article } from "~/services/devto/types";
+
+const config = useAppConfig();
+
+const [protocol, hostname] = config.url.split("//");
+const jobLink = `${protocol}//jobs.${hostname}`;
 
 const { data: articles } = await useFetch<Article[]>(
   "https://dev.to/api/articles"
@@ -21,5 +27,14 @@ const { data: articles } = await useFetch<Article[]>(
 .articles-wrapper {
   display: grid;
   grid-gap: 1rem;
+}
+
+.articles-wrapper .jobs-link {
+  display: block;
+  padding: 0.8rem;
+  text-align: center;
+  background-color: #18181b;
+  color: #fff;
+  border-radius: 0.5rem;
 }
 </style>
